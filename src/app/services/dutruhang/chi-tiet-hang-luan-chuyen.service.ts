3c64fe73cf06c13d40f92715a750c8880ef9616f
binhtt <tuanvmw169@gmail.com>
2024-09-20 14:49:44 +0700

    hàng luôn tuyển

---
+++ src/app/services/dutruhang/chi-tiet-hang-luan-chuyen.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BaseService} from "../base.service";
+import { ResponseData } from '../../models/response-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChiTietHangLuanChuyenService extends BaseService {
+
+  constructor(httpClient: HttpClient) {
+    super(httpClient, 'wnt-lm-dutruhang','/ct-hang-hoa-luan-chuyen');
+  }
+
+  searchPageHangQuanTam(body: any) {
+    const url = `/api/wnt-lm-dutruhang/ct-hang-hoa-luan-chuyen/search-page-hang-quan-tam`;
+    return this.httpClient.post<ResponseData>(url, body).toPromise();
+  }
+
+  searchPageHangDangGiaoDich(body: any) {
+    const url = `/api/wnt-lm-dutruhang/ct-hang-hoa-luan-chuyen/search-page-hang-giao-dich`;
+    return this.httpClient.post<ResponseData>(url, body).toPromise();
+  }
+}
